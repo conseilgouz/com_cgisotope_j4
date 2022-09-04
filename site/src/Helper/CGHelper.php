a20e8b7fca1c059db343547132664c15e7c1aa62
pmleconte <pascal.leconte@conseilgouz.com>
2022-09-04 18:52:44 +0200

    3.0.1
    Offcanvas range filter

--- site/src/Helper/CGHelper.php
+++ site/src/Helper/CGHelper.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * CG Isotope Component  - Joomla 4.x Component 
-* Version			: 3.0.0
+* Version			: 3.0.1
 * Package			: CG ISotope
 * copyright 		: Copyright (C) 2022 ConseilGouz. All rights reserved.
 * license    		: http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
